5a35ec5d24ab0587694ac60936c0fa91e1fc3edf
katene30 <katenejack@gmail.com>
2022-01-18 14:34:26 +1300

    Initial commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Stacey','Beth','Paru', 'nuts', 'Ur mom']
+for name in girls:
+    hi(name)
+    print('Next chick')
+
+for i in range(1,6):
+    print(i)
